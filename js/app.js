38042a20b1a4b8c79359a6034f8cf66ef5960f90
Cc <1625191507@qq.com>
2022-03-15 22:36:59 +0800

    [2-7]:configure tsconfig.json”

---
+++ js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function startGame() {
+    var msgElement = document.getElementById('messages');
+    msgElement.innerHTML = 'Welcome, My Sean!';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
