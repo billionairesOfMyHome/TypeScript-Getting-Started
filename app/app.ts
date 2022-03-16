6cccc1742752a485a5384937cbbe9db95d7cb8cb
Cc <1625191507@qq.com>
2022-03-16 20:04:08 +0800

    [3-9]:control flow type

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,15 @@ function startGame() {
     let playName: string = 'Audrey';
     //playName = undefined; TS2322: Type 'undefined' is not assignable to type 'string'.
     logPlayer(playName)
-    var msgElement = document.getElementById('messages');
+
+    var msgElement: HTMLElement | string = '111';
+    if(typeof msgElement === 'string'){
+        return msgElement; //(local var) msgElement: string
+    }else {
+        return msgElement; //(local var) msgElement: never
+    }
+    msgElement = document.getElementById('messages');
+    // 不能将类型“HTMLElement | null”分配给类型“string | HTMLElement”。
     msgElement!.innerHTML = 'Welcome, My Sean!'
 }
 // starting a game
