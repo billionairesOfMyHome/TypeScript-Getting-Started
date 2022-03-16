8c9af957350fe657370ad181d41cb286fe6e2c25
Cc <1625191507@qq.com>
2022-03-16 21:40:16 +0800

    [4-3]:no implicit any

--- app/app.ts
+++ app/app.ts
@@ -1,20 +1,18 @@
 function startGame() {
-    let playName: string = 'Audrey';
-    //playName = undefined; TS2322: Type 'undefined' is not assignable to type 'string'.
-    logPlayer(playName)
+    /* let playName: string;
+    TS2322: Type 'undefined' is not assignable to type 'string'.
+     strictNullChecks = true 不能将类型“undefined”分配给类型“string”
+    playName = undefined; 
+    logPlayer(playName) */
 
-    var msgElement: HTMLElement | string = '111';
-    if(typeof msgElement === 'string'){
-        return msgElement; //(local var) msgElement: string
-    }else {
-        return msgElement; //(local var) msgElement: never
-    }
-    msgElement = document.getElementById('messages');
-    // 不能将类型“HTMLElement | null”分配给类型“string | HTMLElement”。
+    
+    var msgElement = document.getElementById('messages');
     msgElement!.innerHTML = 'Welcome, My Sean!'
 }
 // starting a game
-function logPlayer(name) {
+/* "noImplicitAny": true, 不能有隐式的 any , 显式的可以有
+参数“name”隐式具有“any”类型 */
+function logPlayer(name: any) {
     console.log(`New game starting for player:${name}`);
 }
 
