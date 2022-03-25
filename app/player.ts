3b24579d38bac85da1b0265c21e6ccea20fb57d5
Sean <1625191507@qq.com>
2022-03-25 23:04:02 +0800

    [5-8]:create class

---
+++ app/player.ts
@@ -0,0 +1,13 @@
+/// <reference path="person.ts"/>
+
+class Player implements IPerson {
+    name: string;
+    age?: number;
+    highScore?: number;
+
+    formatName() {
+        return this.name.toUpperCase();
+    }
+}
+
+// 类会编译成实际有内容的文件
